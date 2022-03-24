5f0f323caf220259139ecd9db6edeea4db859923
DrFreud94 <ls044488@gmail.com>
2022-03-24 18:28:32 +0100

    fix

--- disastrOS_message_queue_read.c
+++ disastrOS_message_queue_read.c
@@ -27,13 +27,6 @@ void internal_message_queue_read() {
 
     MessageQueue* mq = (MessageQueue*)resource;
     if(mq->msgs.size == 0) {
-        // running->status = Waiting;
-        // List_insert(&waiting_list, waiting_list.last, (ListItem*)running);
-        // List_insert(&mq->writing_pids, mq->writing_pids.last, (ListItem*) PCBPtr_alloc(running));
-
-        // PCB* next = (PCB*)List_detach(&ready_list, ready_list.first);
-        // next->status = Running;
-        // running = next;
         if(mq->writing_pid != NULL) {
             running->syscall_retvalue = DSOS_WAITINGFORMESSAGE;
         } else {
@@ -56,13 +49,3 @@ void internal_message_queue_read() {
     List_detach(&mq->msgs, (ListItem*)m);
     assert(m_free(m)>=0);
 }
-    // while(mq->writing_pids.size > 0) {
-    //     PCBPtr* process = (PCBPtr*)List_detach(&mq->writing_pids, mq->writing_pids.first);
-    //     PCB* pcb = process->pcb;
-
-    //     pcb->status = Ready;
-    //     pcb->return_value = DSOS_MQ_CONTINUE;
-    //     List_insert(&ready_list, ready_list.last, (ListItem*) pcb);
-
-    //     assert(PCBPtr_free(process)>=0);
-    // }
